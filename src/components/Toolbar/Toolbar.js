c3dab0eac219a2c235a9d8ab896afe728629500b
Jujuskova <julie.lucanik@gmail.com>
2018-11-28 20:59:25 +0100

    navbar tuto beginning

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './toolbar.css';
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
+
+const Toolbar = (props) => (
+    <header className="toolbar">
+        <nav className="toolbar_navigation">
+            <div>
+                <DrawerToggleButton />
+            </div>
+            <div className="toolbar_logo"><a href="/" >The LOGO</a></div>
+            <div className="spacer"></div>
+            <div className="toolbar_navigation-items">
+                <ul>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Users</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default Toolbar;
